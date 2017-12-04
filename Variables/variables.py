826907318612c39e7b1e3dc463c2bf9569c59c7d
Iwifia <iwifia@live.com>
2017-12-03 19:21:16 -0500

    Initial

---
+++ Variables/variables.py
@@ -0,0 +1,26 @@
+_author_ = 'Corey'
+
+greeting = "Bruce"
+
+_myName = "Tim"
+
+#45Time = "Cannot start with number"
+
+Tim45 = "Good"
+
+Tim_Was_57 = "Hello"
+
+Greeting = "Upper case and different than lower case greeting"
+
+print(Tim_Was_57 + ' ' + greeting)
+
+#age = 24  This does not work due to it being an integer out of quotes
+
+age = "24" # This works because it is built as a string
+
+print(age)
+
+print(greeting + age)
+
+a = 12
+
